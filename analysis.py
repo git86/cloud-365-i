3d3827c22dd7e5e621e27c2d0defbd94a953f5cc
git86 <torrifictod@yahoo.com>
2022-09-08 16:42:39 +0100

    Updated README with new features

--- analysis.py
+++ analysis.py
@@ -9,3 +9,4 @@ Temporary commit content for 2022-08-24T16:42:39
 Temporary commit content for 2022-08-28T16:42:39
 Temporary commit content for 2022-08-30T16:42:39
 Temporary commit content for 2022-09-01T16:42:39
+Temporary commit content for 2022-09-08T16:42:39
