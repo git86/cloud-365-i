cbb64165e1717f09450949c618ea53fb6da27ab8
git86 <torrifictod@yahoo.com>
2022-09-18 16:42:39 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -3,3 +3,4 @@ Temporary commit content for 2022-06-24T16:42:39
 Temporary commit content for 2022-06-30T16:42:39
 Temporary commit content for 2022-08-08T16:42:39
 Temporary commit content for 2022-08-21T16:42:39
+Temporary commit content for 2022-09-18T16:42:39
