ec3a3001141a7439870e5267f9755ca7ed6ab4c8
git86 <torrifictod@yahoo.com>
2023-09-05 16:42:39 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -26,3 +26,4 @@ Temporary commit content for 2023-05-18T16:42:39
 Temporary commit content for 2023-06-01T16:42:39
 Temporary commit content for 2023-06-17T16:42:39
 Temporary commit content for 2023-07-15T16:42:39
+Temporary commit content for 2023-09-05T16:42:39
