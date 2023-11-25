b24dc5e3ec50a5b5116e3e023f6ee2a0cbaea4c8
git86 <torrifictod@yahoo.com>
2023-11-25 16:42:39 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -32,3 +32,4 @@ Temporary commit content for 2023-10-15T16:42:39
 Temporary commit content for 2023-10-28T16:42:39
 Temporary commit content for 2023-10-31T16:42:39
 Temporary commit content for 2023-11-06T16:42:39
+Temporary commit content for 2023-11-25T16:42:39
