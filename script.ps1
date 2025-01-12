8d8a4761f8b3aacfdac441a687a86b495c146536
git86 <torrifictod@yahoo.com>
2025-01-12 16:42:39 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -69,3 +69,4 @@ Temporary commit content for 2024-12-15T16:42:39
 Temporary commit content for 2024-12-22T16:42:39
 Temporary commit content for 2024-12-30T16:42:39
 Temporary commit content for 2025-01-05T16:42:39
+Temporary commit content for 2025-01-12T16:42:39
